847c2bfe9177420c4e6a71e4174264ff32c8dc33
Grace Ren <grac3r3nn@gmail.com>
2021-07-21 18:27:58 -0400

    Added buttons to recycling guide

--- GRRR/SecondViewController.swift
+++ GRRR/SecondViewController.swift
@@ -15,7 +15,141 @@ class SecondViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func bottleButtonTapped(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Plastic Bottle", message: "It can be recycled, but make sure to dry it and put the cap back on!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func magazineButtonTapped(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Magazine", message: "It can be recycled, but make sure to dry it and put the cap back on!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func bagButtonTapped(_ sender: Any) {
+        
+        let alertController = UIAlertController(title: "Plastic Bag", message: "It can be recycled, but make sure to dry it and put the cap back on!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func foamButtonTapped(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Polystyrene Foam", message: "It can be recycled, but make sure to dry it and put the cap back on!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func cartonButtonTapped(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Milk Carton", message: "It can be recycled, but make sure to dry it and put the cap back on!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func shirtButtonTapped(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Clothes", message: "It can be recycled, but make sure to dry it and put the cap back on!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func glassButtonTapped(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Glass Bottle", message: "It can be recycled, but make sure to dry it and put the cap back on!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func cardboardButtonTapped(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Cardboard", message: "It can be recycled, but make sure to dry it and put the cap back on!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func shampooButtonTapped(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Shampoo Bottle", message: "It can be recycled, but make sure to dry it and put the cap back on!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func hoseButtonTapped(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Garden Hose", message: "It can be recycled, but make sure to dry it and put the cap back on!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func canButtonTapped(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Aluminum Can", message: "It can be recycled, but make sure to dry it and put the cap back on!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func tireButtonTapped(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Tire", message: "It can be recycled, but make sure to dry it and put the cap back on!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func diaperButtonTapped(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Diaper", message: "It can be recycled, but make sure to dry it and put the cap back on!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func hangerButtonTapped(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Clothes Hanger", message: "It can be recycled, but make sure to dry it and put the cap back on!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func cheerioButtonTapped(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "Cereal Box", message: "It can be recycled, but make sure to dry it and put the cap back on!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
